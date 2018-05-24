3d96f574bc96f843cc42cbefaed49eaf71de1d1f
Edern76 <gawein.le@live.fr>
2018-05-24 12:59:34 +0200

    Ajouté début de code d'interface

--- GUI.pas
+++ GUI.pas
@@ -1,10 +1,18 @@
 unit GUI;
 
 interface
-uses Common;
+uses Common, sdl, sdl_image;
+
+function InitRender() : PSDL_SURFACE;
 
 implementation
 
+function InitRender() : PSDL_SURFACE;
+	BEGIN
+	SDL_Init(SDL_INIT_VIDEO);
+	
+	InitRender := SDL_SETVIDEOMODE(1280, 720, 32, SDL_HWSURFACE);
+	END;
 
 END.
 
--- Main.pas
+++ Main.pas
@@ -1,9 +1,14 @@
 program Main;
 
-uses GameLogic, GUI, Common;
+uses GameLogic, GUI, Common, sdl, sdl_image;
+
+
+var window : PSDL_SURFACE;
 
 BEGIN
-	
-	
+window := InitRender();
+SDL_Flip(window);
+SDL_delay(4000);
+
 END.
 
