2f11fce6788b455193dc96cc633eb66b7cf3e3e7
milouwii <milouwii@live.fr>
2018-05-30 12:27:31 +0200

    Modifie retournage de cartes

--- GameLogic.pas
+++ GameLogic.pas
@@ -1,47 +1,58 @@
 unit GameLogic;
 
 interface
+
 uses Common;
 
-procedure RetourneCarte(j : Integer; var carte : TCard);
+procedure RetourneCarte(var carte : TCard; var t : Grid);
 
-function Victoire(g : Grid) : Boolean;
+function Victoire(i,j : Integer; g : Grid) : Boolean;
 
 
-var cartePrecedente : TCard ;
+var cartePrecedente : TCard ; //Faudrait initialiser avec une valeur bullshit
 	nbPaire : Integer;
 
 implementation
 
 {Retourne une carte et verifie si une paire a ete formee}
-procedure RetourneCarte(j : Integer; var carte: TCard);
-
-var c : TCard;
-var i : Integer;
-
-
+procedure RetourneCarte(var carte: TCard;  var t : Grid);
+	var x,y : Integer;
+	
 	BEGIN
-		i:=0;
-		if i=0 then
+		if (not carte.Selected) then
 			BEGIN
-			inc(i);
-			END
-		else
-			BEGIN
-			if i=1 then
-				carte.Selected := True;
+			carte.Selected := True;
+			writeln('Carte retournee');
+			END;
 			
-			if carte.CardType = cartePrecedente.CardType then
-				begin
-				end;
-			END
+		if carte.CardType = cartePrecedente.CardType then
+			begin
+				nbPaire := nbPaire + 1;
+				carte.Revealed := True;
+				x := cartePrecedente.x;
+				y := cartePrecedente.y;
+				t[x][y].Revealed := True;
+				
+			end;
+			
 		
 	END;
 
 {Verifie si la partie est gagnée, c'est-a-dire, si toutes les cartes sont retournees}
-function Victoire(g : Grid) : Boolean;
+function Victoire(i,j : Integer; g : Grid) : Boolean;
 	BEGIN
-	
+	i:=0; j:=0;
+	repeat
+		inc(i);
+		repeat
+			inc(j);
+			
+		until j = GetDim;
+			
+	until (g[i][j].Revealed = False) OR ((i=GetDim()) AND (j=GetDim()));
+					
+			
+			
 	END;
 	
 	
